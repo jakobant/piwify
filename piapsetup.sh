dc71c62fd237b507ce6b711cadffb19399e54807
Jakob Antonsson <jakobant@gmail.com>
2017-11-29 21:44:08 +0000

    Fix for pi

--- piapsetup.sh
+++ piapsetup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-
-sudo ifconfig wlan0 up 192.168.191.1 netmask 255.255.255.0
-sleep 5
-sudo dnsmasq -C ./dnsmasq.conf
-
-AP="pi_`ip add show wlan0 |awk '/ether/{print $2}'|sed 's/://g'`"
-PASS=`tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1`
-PASS="b123456789"
-
-cat hostapd.conf | sed "s/NETNAME/${AP}/g;s/NETPASS/${PASS}/g" > /tmp/hostapd.conf
-DISPLAY=:0 zenity --warning --no-wrap --text="Wifi password for $AP set to : $PASS\nConnect to http://192.168.1.191.1:5678 to setup your Pi" &
+SIP=${PI_IP_NET:-192.168.11}
 
 sudo killall -9 /sbin/dhcpcd
+sudo killall -9 dnsmasq
+
+sudo ifconfig wlan0 up ${SIP}.1 netmask 255.255.255.0
+sleep 5
+
+cat dnsmasq.conf.template | sed "s//SIP/g" > /tmp/dnsmasq.conf
+sudo dnsmasq -C /tmp/dnsmasq.conf
+
+AP="pi_`ip add show wlan0 |awk '/ether/{print $2}'|sed 's/://g'`"
+PASS=`tr -cd '[:alnum:]' < /dev/urandom | fold -w8 | head -n1`
+
+cat hostapd.conf.template | sed "s/NETNAME/${AP}/g;s/NETPASS/${PASS}/g" > /tmp/hostapd.conf
+DISPLAY=:0 zenity --warning --no-wrap --text="Wifi password for $AP set to : $PASS\nConnect to http://${SIP}.1:5678 to setup your Pi" &
 
 sudo hostapd /tmp/hostapd.conf
